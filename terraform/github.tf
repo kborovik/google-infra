479f96257fc5bab30f02fc10ec7041906c34a009
Konstantin Borovik <kb@lab5.ca>
2024-07-29 00:07:56 -0400

    2024.07.29-0007

---
+++ terraform/github.tf
@@ -0,0 +1,61 @@
+###############################################################################
+# GitHub OpenID Connect Provider
+###############################################################################
+
+locals {
+  assertion_aud = "https://github.com/kborovik"
+  assertion_sub = "repo:kborovik/google-infra:environment:${var.google_project}"
+}
+
+resource "google_iam_workload_identity_pool" "github" {
+  display_name              = "github"
+  workload_identity_pool_id = "github"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github" {
+  display_name                       = "github"
+  workload_identity_pool_provider_id = "github"
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
+  attribute_mapping = {
+    "attribute.actor"      = "assertion.actor"
+    "attribute.aud"        = "assertion.aud"
+    "attribute.repository" = "assertion.repository"
+    "google.subject"       = "assertion.sub"
+  }
+  oidc {
+    allowed_audiences = ["${local.assertion_aud}"]
+    issuer_uri        = "https://token.actions.githubusercontent.com"
+  }
+}
+
+###############################################################################
+# GitHub - Service Account Mapping
+###############################################################################
+
+resource "google_service_account" "github" {
+  account_id   = "github"
+  display_name = "GitHub OIDC"
+}
+
+locals {
+  github_aim_roles = [
+    "roles/compute.admin",
+    "roles/iam.workloadIdentityUser",
+    "roles/networkmanagement.admin",
+  ]
+}
+
+resource "google_project_iam_member" "github" {
+  count   = length(local.github_aim_roles)
+  project = var.google_project
+  role    = local.github_aim_roles[count.index]
+  member  = "serviceAccount:${google_service_account.github.email}"
+}
+
+resource "google_service_account_iam_binding" "github" {
+  service_account_id = google_service_account.github.name
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.sub/${local.assertion_sub}",
+  ]
+}
