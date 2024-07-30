a7d1d50e23d602e07cd646e7c22ff3ca091c574f
Konstantin Borovik <kb@lab5.ca>
2024-07-30 19:01:02 -0400

    2024.07.30-1901

--- terraform/github.tf
+++ terraform/github.tf
@@ -28,31 +28,13 @@ resource "google_iam_workload_identity_pool_provider" "github" {
 }
 
 ###############################################################################
-# GitHub - Service Account Mapping
+# GitHub - principalSet IAM
 ###############################################################################
-
-resource "google_service_account" "github" {
-  display_name = "GitHub OIDC"
-  account_id   = "github"
-}
-
-resource "google_service_account_iam_member" "github" {
-  service_account_id = google_service_account.github.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${local.assertion_repository}"
-}
 
 locals {
   github_aim_roles = [
     "roles/editor",
   ]
-}
-
-resource "google_project_iam_member" "github_service_account" {
-  count   = length(local.github_aim_roles)
-  project = var.google_project
-  role    = local.github_aim_roles[count.index]
-  member  = "serviceAccount:${google_service_account.github.email}"
 }
 
 resource "google_project_iam_member" "github_principal_set" {
